c29e7b61d27196d04d0409437e28be905571bf76
Łukasz Chrzanowski <deancraft@gmail.com>
2019-06-30 10:44:13 +0200

    Added some servlets and views

---
+++ src/main/java/com/isa/eshop/servlets/Page2Servlet.java
@@ -0,0 +1,44 @@
+package com.isa.eshop.servlets;
+
+import com.isa.eshop.config.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Logger;
+
+@WebServlet("/page2")
+public class Page2Servlet extends HttpServlet {
+
+    Logger logger = Logger.getLogger(getClass().getName());
+
+    private static final String TEMPLATE_NAME = "index";
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.addHeader("Content-Type", "text/html; charset=utf-8");
+
+        Map<String, Object> model = new HashMap<>();
+        model.put("content", "page2");
+
+        Template template = templateProvider.getTemplate(
+                getServletContext(), TEMPLATE_NAME
+        );
+
+        try {
+            template.process(model, response.getWriter());
+        } catch (TemplateException e) {
+            System.err.println("Error while processing template: " + e);
+        }
+    }
+}
